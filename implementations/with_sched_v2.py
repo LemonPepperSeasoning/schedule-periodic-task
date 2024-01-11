e52f7c7a18d0e7f205a22874cf0d747c5f0dbfca
LemonPepperSeasoning <takahiro.swe@gmail.com>
2024-01-11 20:05:25 +0900

    new implementation

---
+++ implementations/with_sched_v2.py
@@ -0,0 +1,31 @@
+import sched, time, threading
+from task import task, TASK_FREQUENCY, TASK_PRIORITY
+
+
+def main():
+    
+    LAG = 2
+
+    start_time = time.monotonic()
+    counter = 0
+
+    while True:
+        s = sched.scheduler(time.monotonic, time.sleep)
+        
+        for _ in range(5):
+            s.enterabs(
+                start_time + counter + LAG,
+                TASK_PRIORITY,
+                task,
+                argument=(counter,)
+            )
+
+            counter += TASK_FREQUENCY
+
+        scheduler_thread = threading.Thread(target=s.run)
+        scheduler_thread.start()            
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
