ce53077321b5042807d1d08a03971763e13d28df
LemonPepperSeasoning <takahiro.swe@gmail.com>
2024-01-13 03:50:13 +1300

    fixed lint

--- implementations/with_schedule.py
+++ implementations/with_schedule.py
@@ -1,15 +1,12 @@
-import schedule, time
+import schedule
 from task import task, TASK_FREQUENCY
 
 
 def main():
-    #schedule.every().day.at('13:56').do(sched_job)
     schedule.every(TASK_FREQUENCY).seconds.do(lambda: task("my param"))
-
     while True:
         schedule.run_pending()
 
 
 if __name__ == "__main__":
     main()
-
