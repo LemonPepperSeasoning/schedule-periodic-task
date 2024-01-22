76ec7312f3fa29b5310b2925842499861f9c6664
LemonPepperSeasoning <takahiro.swe@gmail.com>
2024-01-23 01:07:12 +0900

    changed to threadpool

--- implementations/with_manual_scheduling_v3_thread.py
+++ implementations/with_manual_scheduling_v3_thread.py
@@ -1,18 +1,25 @@
 import time, threading
 from task import task, TASK_FREQUENCY
+from concurrent.futures import ThreadPoolExecutor
+import os
+
 
 
 def main():
     counter = 0
     start_time = round(time.monotonic())
+    
+    print(f"Creating threadpool of size #{os.cpu_count()}")
 
-    while True:
-        threading.Thread(target=task, args=(counter,)).start()
-        elapsed_time = time.monotonic() - start_time - counter
-        sleep_time = max(0, TASK_FREQUENCY - elapsed_time)
-        time.sleep(sleep_time)
-        counter += 1
+    with ThreadPoolExecutor() as executor:
+        while True:
+            _ = executor.submit(task, args=(counter,))
+            elapsed_time = time.monotonic() - start_time - counter
+            sleep_time = max(0, TASK_FREQUENCY - elapsed_time)
+            time.sleep(sleep_time)
+            counter += 1
 
 
 if __name__ == "__main__":
     main()
+
